dc6bd185352ad7aee86313a7093bb7203509ee32
Yonn-Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2024-08-29 14:52:15 +0900

    feat: create dropdown menu

--- src/features/editor/components/navbar.tsx
+++ src/features/editor/components/navbar.tsx
@@ -1,9 +1,57 @@
 'use client';
 
+import Hint from '@/components/hint';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Separator } from '@/components/ui/separator';
+import { ChevronDown, MousePointerClick } from 'lucide-react';
+import { CiFileOn } from 'react-icons/ci';
+import Logo from './logo';
+
 export default function Navbar() {
   return (
     <nav className="w-full flex items-center p-4 h-[68px] gap-x-8 border-b lg:pl-[34px]">
-      <span>navbar</span>
+      <Logo />
+      <div className="w-full flex items-center gap-x-1 h-full">
+        <DropdownMenu modal={false}>
+          <DropdownMenuTrigger asChild>
+            <Button size="sm" variant="ghost">
+              File
+              <ChevronDown className="size-4 ml-2" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start" className="min-w-60">
+            <DropdownMenuItem
+              onClick={() => {}} //TODO: add function for opening file
+              className="flex items-center gap-x-2 cursor-pointer"
+            >
+              <CiFileOn className="size-8" />
+              <div>
+                <p>Open</p>
+                <p className="text-xs text-muted-foreground">
+                  Open a JSON file
+                </p>
+              </div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Separator orientation="vertical" className="mx-2" />
+        <Hint label="Select" side="bottom">
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => {}}
+            className="" //TODO: add dynamic class
+          >
+            <MousePointerClick className="size-5" />
+          </Button>
+        </Hint>
+      </div>
     </nav>
   );
 }
