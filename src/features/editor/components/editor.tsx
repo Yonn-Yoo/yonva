6c7b723bfa28c64aa9ce2f5218e0dfc0998399c9
Yonn-Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2024-08-29 17:36:31 +0900

    feat: create sidebar item

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { fabric } from 'fabric';
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import useEditor from '../hooks/use-editor';
+import { ToolType } from '../types';
 import Footer from './footer';
 import Navbar from './navbar';
 import Sidebar from './sidebar';
@@ -12,6 +13,7 @@ export default function Editor() {
   const { init } = useEditor();
   const canvasRef = useRef(null);
   const containerRef = useRef(null);
+  const [activeTool, setActiveTool] = useState<ToolType>('select');
 
   useEffect(() => {
     const canvas = new fabric.Canvas(canvasRef.current, {
@@ -29,11 +31,33 @@ export default function Editor() {
     };
   }, [init]);
 
+  const onChangeActiveTool = useCallback(
+    (tool: ToolType) => {
+      if (tool === 'draw') {
+        // editor?.enableDrawingMode();
+      }
+
+      if (activeTool === 'draw') {
+        // editor?.disableDrawingMode();
+      }
+
+      if (tool === activeTool) {
+        return setActiveTool('select');
+      }
+
+      setActiveTool(tool);
+    },
+    [activeTool]
+  );
+
   return (
     <div className="h-full flex flex-col">
       <Navbar />
       <div className="absolute top-[68px] h-[calc(100%-68px)] w-full flex">
-        <Sidebar />
+        <Sidebar
+          onChangeActiveTool={onChangeActiveTool}
+          activeTool={activeTool}
+        />
         <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
           <Toolbar />
           <div
--- src/features/editor/types.ts
+++ src/features/editor/types.ts
@@ -4,3 +4,20 @@ export type UseEditorInitArgType = {
   initialCanvas: fabric.Canvas;
   initialContainer: HTMLDivElement;
 };
+
+export type ToolType =
+  | 'select'
+  | 'shapes'
+  | 'text'
+  | 'images'
+  | 'draw'
+  | 'fill'
+  | 'stroke-color'
+  | 'stroke-width'
+  | 'font'
+  | 'opacity'
+  | 'filter'
+  | 'settings'
+  | 'ai'
+  | 'remove-bg'
+  | 'templates';
