983bf07d9265899200888e613f428a31b6790db1
Yeonseung Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2024-09-18 17:12:03 +0900

    filter by template

--- src/app/(dashboard)/projects-section.tsx
+++ src/app/(dashboard)/projects-section.tsx
@@ -88,62 +88,65 @@ export const ProjectsSection = () => {
       <ConfirmDialog />
       <h3 className="text-[#262E3C] font-extrabold text-lg">{`${name}'s recent projects`}</h3>
       <section className="w-full !text-[#262E3C] border-separate">
-        {data.pages.map((group, idx) => (
-          <div className="w-full flex flex-col space-y-3.5" key={idx}>
-            {group.data.map((project) => (
-              <div
-                className="w-full flex justify-between items-center bg-slate-50 py-4 px-6 rounded-lg duration-300 hover:!shadow-gray-200 hover:shadow-md hover:scale-[101%] group"
-                key={project.id}
-              >
+        {data.pages.map((group, idx) => {
+          const formattedData = group.data.filter((d) => !d.isTemplate);
+          return (
+            <div className="w-full flex flex-col space-y-3.5" key={idx}>
+              {formattedData.map((project) => (
                 <div
-                  onClick={() => router.push(`/editor/${project.id}`)}
-                  className="w-full md:w-[30%] h-10 font-medium flex items-center space-x-3 cursor-pointer"
+                  className="w-full flex justify-between items-center bg-slate-50 py-4 px-6 rounded-lg duration-300 hover:!shadow-gray-200 hover:shadow-md hover:scale-[101%] group"
+                  key={project.id}
                 >
-                  <Presentation className="size-5" />
-                  <span className="w-fit line-clamp-1 relative">
-                    {project.name}
-                    <div className="w-0 group-hover:w-full h-px bg-black/40 rounded-lg duration-500 ease-in-out" />
-                  </span>
+                  <div
+                    onClick={() => router.push(`/editor/${project.id}`)}
+                    className="w-full md:w-[30%] h-10 font-medium flex items-center space-x-3 cursor-pointer"
+                  >
+                    <Presentation className="size-5" />
+                    <span className="w-fit line-clamp-1 relative">
+                      {project.name}
+                      <div className="w-0 group-hover:w-full h-px bg-black/40 rounded-lg duration-500 ease-in-out" />
+                    </span>
+                  </div>
+                  <div className="hidden md:table-cell md:w-[18%]">
+                    {project.width} x {project.height} px
+                  </div>
+                  <div className="hidden md:table-cell md:w-[18%]">
+                    {formatDistanceToNow(project.updatedAt, {
+                      addSuffix: true,
+                    })}
+                  </div>
+                  <div className="flex items-center justify-end">
+                    <DropdownMenu modal={false}>
+                      <DropdownMenuTrigger asChild>
+                        <Button disabled={false} size="icon" variant="ghost">
+                          <MoreHorizontal className="size-4 text-gray-400" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end" className="w-60">
+                        <DropdownMenuItem
+                          className="h-10 cursor-pointer"
+                          disabled={duplicateMutation.isPending}
+                          onClick={() => onCopy(project.id)}
+                        >
+                          <CopyIcon className="size-4 mr-2" />
+                          Make a copy
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          className="h-10 cursor-pointer"
+                          disabled={removeMutation.isPending}
+                          onClick={() => onDelete(project.id)}
+                        >
+                          <Trash className="size-4 mr-2" />
+                          Delete
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
                 </div>
-                <div className="hidden md:table-cell md:w-[18%]">
-                  {project.width} x {project.height} px
-                </div>
-                <div className="hidden md:table-cell md:w-[18%]">
-                  {formatDistanceToNow(project.updatedAt, {
-                    addSuffix: true,
-                  })}
-                </div>
-                <div className="flex items-center justify-end">
-                  <DropdownMenu modal={false}>
-                    <DropdownMenuTrigger asChild>
-                      <Button disabled={false} size="icon" variant="ghost">
-                        <MoreHorizontal className="size-4 text-gray-400" />
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end" className="w-60">
-                      <DropdownMenuItem
-                        className="h-10 cursor-pointer"
-                        disabled={duplicateMutation.isPending}
-                        onClick={() => onCopy(project.id)}
-                      >
-                        <CopyIcon className="size-4 mr-2" />
-                        Make a copy
-                      </DropdownMenuItem>
-                      <DropdownMenuItem
-                        className="h-10 cursor-pointer"
-                        disabled={removeMutation.isPending}
-                        onClick={() => onDelete(project.id)}
-                      >
-                        <Trash className="size-4 mr-2" />
-                        Delete
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </div>
-              </div>
-            ))}
-          </div>
-        ))}
+              ))}
+            </div>
+          );
+        })}
       </section>
       {hasNextPage && (
         <div className="w-full flex items-center justify-center pt-4">
--- src/features/projects/api/use-get-projects.ts
+++ src/features/projects/api/use-get-projects.ts
@@ -1,27 +1,29 @@
-import { InferResponseType } from "hono";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { InferResponseType } from 'hono';
 
-import { client } from "@/lib/hono";
+import { client } from '@/lib/hono';
 
-export type ResponseType = InferResponseType<typeof client.api.projects["$get"], 200>;
+export type ResponseType = InferResponseType<
+  (typeof client.api.projects)['$get'],
+  200
+>;
 
 export const useGetProjects = () => {
   const query = useInfiniteQuery<ResponseType, Error>({
     initialPageParam: 1,
     getNextPageParam: (lastPage) => lastPage.nextPage,
-    queryKey: ["projects"],
+    queryKey: ['projects'],
     queryFn: async ({ pageParam }) => {
       const response = await client.api.projects.$get({
         query: {
           page: (pageParam as number).toString(),
-          limit: "5",
+          limit: '5',
         },
       });
 
       if (!response.ok) {
-        throw new Error("Failed to fetch projects");
+        throw new Error('Failed to fetch projects');
       }
-
       return response.json();
     },
   });
